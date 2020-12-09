8cdb0b995ccd83012b0f09d25e9c4d4780455095
vencis <vaclav.fiser@gmail.com>
2020-12-09 11:01:04 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,16 +9,14 @@ fi
 # Graceful shutdown
 trap 'pkill -TERM -P1; electrum stop; exit 0' SIGTERM
 
+# Run application
+electrum $FLAGS daemon -d
+
 # Set config
 electrum $FLAGS setconfig rpcuser ${ELECTRUM_USER}
 electrum $FLAGS setconfig rpcpassword ${ELECTRUM_PASSWORD}
 electrum $FLAGS setconfig rpchost 0.0.0.0
 electrum $FLAGS setconfig rpcport 7000
-
-# XXX: Check load wallet or create
-
-# Run application
-electrum $FLAGS daemon -d
 
 # Loading wallet if exists
 if [ -e "/home/electrum/.electrum/wallets/default_wallet" ]
